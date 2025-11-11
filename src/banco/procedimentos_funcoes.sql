fc2bc96c7c62610c6abafbd48dd14f2a1a4c774d
gmarques167 <guimlucena1201@gmail.com>
2025-11-10 21:24:37 -0300

    Update procedimentos_funcoes.sql

--- src/banco/procedimentos_funcoes.sql
+++ src/banco/procedimentos_funcoes.sql
@@ -1,8 +1,8 @@
 USE ecommerce;
 
---Procedimentos e funções
 
 DELIMITER $$
+
 CREATE FUNCTION calcula_idade(p_id INT)
 RETURNS INT
 DETERMINISTIC
@@ -11,8 +11,7 @@ BEGIN
    SELECT dataNascimento INTO v_dataNasc FROM Cliente WHERE id = p_id;
    RETURN TIMESTAMPDIFF(YEAR, v_dataNasc, CURDATE());
 END$$
-DELIMITER ;
-DELIMITER $$
+
 CREATE FUNCTION soma_fretes(p_destino VARCHAR(50))
 RETURNS DOUBLE
 DETERMINISTIC
@@ -21,8 +20,7 @@ BEGIN
    SELECT SUM(valorFrete) INTO total FROM CompraVenda WHERE destino = p_destino;
    RETURN IFNULL(total, 0);
 END$$
-DELIMITER ;
-DELIMITER $$
+
 CREATE FUNCTION arrecadado(p_data DATE, p_idVendedor INT)
 RETURNS DOUBLE
 DETERMINISTIC
@@ -34,46 +32,44 @@ BEGIN
    WHERE cv.idVendedor = p_idVendedor AND cv.data = p_data;
    RETURN IFNULL(total, 0);
 END$$
+
 DELIMITER ;
 
 
 DELIMITER //
-CREATE PROCEDURE reajuste_salario(IN percentual DOUBLE, IN categoria VARCHAR(20))
+
+CREATE PROCEDURE reajuste_salario(IN percentual DOUBLE, IN categoria VARCHAR(50))
 BEGIN
-    UPDATE Vendedor
-    SET nota = nota + (nota * (percentual / 100))
-    WHERE tipo = categoria;
+    UPDATE Vendedor v
+    JOIN Cargo c ON v.idCargo = c.id
+    SET v.salario = v.salario + (v.salario * (percentual / 100))
+    WHERE c.nome = categoria;
 
     INSERT INTO Notificacao (mensagem)
     VALUES (CONCAT('Reajuste de ', percentual, '% aplicado à categoria ', categoria));
 END //
-DELIMITER ;
-
-DELIMITER //
+  
 CREATE PROCEDURE sorteio_cliente()
 BEGIN
     DECLARE cliente_id INT;
-    DECLARE valor_voucher DOUBLE;
+    DECLARE valor_voucher DOUBLE DEFAULT 100;
 
     SELECT id INTO cliente_id
     FROM Cliente
     ORDER BY RAND()
     LIMIT 1;
 
-    IF cliente_id IN (SELECT id FROM ClienteEspecial) THEN
-        SET valor_voucher = 200;
-    ELSE
-        SET valor_voucher = 100;
-    END IF;
-
     INSERT INTO Notificacao (mensagem)
     VALUES (CONCAT('Cliente sorteado (ID ', cliente_id, ') recebeu um voucher de R$ ', valor_voucher));
 END //
-DELIMITER ;
-
-DELIMITER //
-CREATE PROCEDURE registrar_venda(IN id_produto INT)
+  
+CREATE PROCEDURE registrar_venda(IN id_produto INT, IN id_cliente INT, IN id_vendedor INT, IN destino VARCHAR(50), IN valorFrete DOUBLE)
 BEGIN
+    -- Registra a venda
+    INSERT INTO CompraVenda (idProduto, idCliente, idVendedor, data, destino, valorFrete)
+    VALUES (id_produto, id_cliente, id_vendedor, CURDATE(), destino, valorFrete);
+
+    -- Atualiza o estoque do produto
     UPDATE Produto
     SET quantidade = quantidade - 1
     WHERE id = id_produto;
@@ -81,9 +77,7 @@ BEGIN
     INSERT INTO Notificacao (mensagem)
     VALUES (CONCAT('Venda registrada - Produto ID ', id_produto, ' teve estoque reduzido.'));
 END //
-DELIMITER ;
 
-DELIMITER //
 CREATE PROCEDURE estatisticas()
 BEGIN
     SELECT 
@@ -98,5 +92,6 @@ BEGIN
     GROUP BY p.id, mes
     ORDER BY total_vendido DESC;
 END //
+
 DELIMITER ;
 
